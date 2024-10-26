9febf281a76deb58de33d50595151800799645d9
devSuryansh <suryanshg2050@gmail.com>
2024-10-26 20:33:56 +0530

    Updates the description

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -134,7 +134,7 @@ const CreatePost = () => {
       <div>
         <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
         <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">
-          Generate an imaginative image AI and share it with the community
+          Bring your imagination to life and share it with the community
         </p>
       </div>
 
